cb1b5fc953cc25f6e652a0056026a8c4b1bb60b6
Darsh <darsh.da.core@gmail.com>
2026-07-15 22:19:02 +0800

    Initial Shadow Hoops game.

--- Shadow Hoops/GameScene.swift
+++ Shadow Hoops/GameScene.swift
@@ -1,88 +1,115 @@
-//
-//  GameScene.swift
-//  Shadow Hoops
-//
-//  Created by Darsh Agarwal on 15/7/26.
-//
-
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+class GameScene: SKScene, SKPhysicsContactDelegate {
+
+    private var ball: SKSpriteNode!
+    private var scoreLabel: SKLabelNode!
+    private var score = 0
+
+    private var dragStart: CGPoint?
+
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+
+        physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
+        physicsWorld.contactDelegate = self
+
+        backgroundColor = .white
+
+        // Ball
+        ball = SKSpriteNode(color: .orange, size: CGSize(width: 50, height: 50))
+        ball.position = CGPoint(x: size.width / 2, y: 120)
+
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 25)
+        ball.physicsBody?.restitution = 0.7
+        ball.physicsBody?.friction = 0.2
+
+        addChild(ball)
+
+        // Backboard
+        let backboard = SKSpriteNode(color: .gray,
+                                     size: CGSize(width: 10, height: 120))
+        backboard.position = CGPoint(x: size.width - 60,
+                                     y: size.height * 0.7)
+
+        backboard.physicsBody = SKPhysicsBody(rectangleOf: backboard.size)
+        backboard.physicsBody?.isDynamic = false
+
+        addChild(backboard)
+
+        // Rim
+        let rim = SKSpriteNode(color: .red,
+                               size: CGSize(width: 70, height: 8))
+
+        rim.position = CGPoint(x: size.width - 100,
+                               y: size.height * 0.65)
+
+        rim.physicsBody = SKPhysicsBody(rectangleOf: rim.size)
+        rim.physicsBody?.isDynamic = false
+
+        addChild(rim)
+
+        // Score Sensor
+        let sensor = SKNode()
+        sensor.position = CGPoint(x: rim.position.x,
+                                  y: rim.position.y - 20)
+
+        sensor.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 60,
+                                                               height: 10))
+        sensor.physicsBody?.isDynamic = false
+        sensor.physicsBody?.categoryBitMask = 2
+        sensor.physicsBody?.contactTestBitMask = 1
+
+        addChild(sensor)
+
+        // Ball category
+        ball.physicsBody?.categoryBitMask = 1
+
+        // Score label
+        scoreLabel = SKLabelNode(fontNamed: "AvenirNext-Bold")
+        scoreLabel.fontSize = 40
+        scoreLabel.fontColor = .black
+        scoreLabel.position = CGPoint(x: size.width/2,
+                                      y: size.height - 80)
+
+        scoreLabel.text = "Score: 0"
+
+        addChild(scoreLabel)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
+    // MARK: Touches
+
+    override func touchesBegan(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+
+        guard let touch = touches.first else { return }
+
+        dragStart = touch.location(in: self)
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
+    override func touchesEnded(_ touches: Set<UITouch>,
+                               with event: UIEvent?) {
+
+        guard let touch = touches.first,
+              let start = dragStart else { return }
+
+        let end = touch.location(in: self)
+
+        let dx = start.x - end.x
+        let dy = start.y - end.y
+
+        ball.physicsBody?.velocity = .zero
+
+        ball.physicsBody?.applyImpulse(
+            CGVector(dx: dx * 0.12,
+                     dy: dy * 0.12)
+        )
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
+    // MARK: Score
+
+    func didBegin(_ contact: SKPhysicsContact) {
+
+        score += 1
+        scoreLabel.text = "Score: \(score)"
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-}
+} 
